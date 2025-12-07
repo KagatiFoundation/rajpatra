38de65c7e77adadc2ffe8d42a02d6c89032fbb42
Ramesh Poudel <boyzdod@gmail.com>
2025-12-07 23:33:03 +0545

    methods to extract content from documents(HTML only for now)

--- crawler/src/main/java/org/kagati/crawler/CrawledDocument.java
+++ crawler/src/main/java/org/kagati/crawler/CrawledDocument.java
@@ -1,6 +1,7 @@
 package org.kagati.crawler;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,14 +15,13 @@ public class CrawledDocument implements FetchResult {
     private final String url;
     private final LocalDateTime fetchedAt;
     private final Map<String, List<String>> metadata;
-    private final String snippet;
 
     public CrawledDocument(Document doc, String url) {
         jsoupDocument = doc;
         this.url = url;
         fetchedAt = LocalDateTime.now();
         metadata = extractMetadata();
-        snippet = generateSnippet();
+        clearNoise();
     }
 
     private Map<String, List<String>> extractMetadata() {
@@ -33,27 +33,21 @@ public class CrawledDocument implements FetchResult {
             String content = metaTag.attr("content");
 
             if (!name.isEmpty()) {
-                if (metadata.containsKey(name)) {
-                    metadata.get(name).add(content);
-                }
-                else {
-                    metadata.put(name.toLowerCase(), List.of(content));
-                }
+                name = name.toLowerCase();
+                metadata.computeIfAbsent(name, k -> new ArrayList<>()).add(content);
             }
             else if (!property.isEmpty()) {
-                if (metadata.containsKey(property)) {
-                    metadata.get(property).add(content);
-                }
-                else {
-                    metadata.put(property.toLowerCase(), List.of(content));
-                }
+                property = property.toLowerCase();
+                metadata.computeIfAbsent(property, k -> new ArrayList<>()).add(content);
             }
         }
         return metadata;
     }
 
-    private String generateSnippet() {
-        return "";
+    private void clearNoise() {
+        jsoupDocument.select("header, footer, nav, script, noscript, aside, link").remove();
+        // remove ad banners
+        jsoupDocument.select("div[class*=ads], div[class*=banner]").remove();
     }
 
     /**
@@ -95,9 +89,48 @@ public class CrawledDocument implements FetchResult {
         else return "website"; // always fallback to 'website'
     }
 
-    public String getSnippet() {
-        return snippet;
+    public String getAllContent() {
+        StringBuilder builder = new StringBuilder(getMainContent());
+        builder.append("\n").append(getArticleContent());
+        if (builder.isEmpty()) {
+            builder.append(getFallbackContent());
+        }
+        return builder.toString();
     }
+
+    public String getMainContent() {
+        StringBuilder mainContent = new StringBuilder();
+        Element mainTag = jsoupDocument.selectFirst("main");
+        if (mainTag != null) {
+            mainContent.append(mainTag.text()).append("\n");
+        }
+        return mainContent.toString();
+    }
+
+    private String getArticleContent() {
+        StringBuilder mainContent = new StringBuilder();
+        Elements articleTags = jsoupDocument.select("article");
+        for (Element article: articleTags) {
+            mainContent.append(article.text()).append("\n");
+        }
+        return mainContent.toString();
+    }
+
+    // heuristic approach
+    private String getFallbackContent() {
+        StringBuilder content = new StringBuilder();
+        Elements divs = jsoupDocument.select("div[id*=content], div[class*=content], section[class*=body]");
+        for (Element div : divs) {
+            content.append(div.text()).append("\n");
+        }
+        return content.toString();
+    }
+
+    public String getSnippet() {
+        String content = getAllContent();
+        return content.length() > 200 ? content.substring(0, 200) : content;
+    }
+
 
     public List<String> getMetadata(String name) {
         return metadata.get(name);
